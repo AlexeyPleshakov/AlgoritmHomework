9ed603f4bfcf963245e25e9e5463c38f8e2345cc
Алексей Плешаков <pleshakov-aleks@yandex.ru>
2024-05-20 21:28:34 +0400

    Добавьте файлы проекта.

---
+++ Algoritm/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public static void Greetings(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                if (user.IsPremium)
+                {
+                    Console.WriteLine($"Hello, {user.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello, {user.Name}");
+                    Program.ShowAds();
+                }
+            }
+        } 
+    }
+}
